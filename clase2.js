c7ffafe487a445dfc3d183cfba1bd6c12d90ec18
YapuraMauro <56448426+YapuraMauro@users.noreply.github.com>
2019-10-31 13:45:27 -0300

    clase 2 - Strings
    Definiendo variables, Nombre/apellidos con mayúscula y minúscula con interpolación de texto.

---
+++ clase2.js
@@ -0,0 +1,10 @@
+var nombre = 'Mauro', apellido = 'Yapura'
+
+var nombreEnMayuscula = nombre.toUpperCase()
+
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.length
+var nombreCompleto = nombre + ' ' + apellido
+
